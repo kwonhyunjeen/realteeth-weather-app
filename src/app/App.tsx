1fe8fbddba30c92ab725c921c1010e0fe4053cf7
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2026-05-11 19:13:26 +0900

    refactor: reorganize app entry files into fsd structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { cn } from './utils/className';
+import { cn } from '../shared/lib/className';
 
 export const App = () => {
   return <h1 className={cn('text-3xl font-bold underline')}>Weather App</h1>;
--- src/specs/사용자는_목적을_위해_행동하기를_원한다.spec.tsx
+++ src/specs/사용자는_목적을_위해_행동하기를_원한다.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import { App } from '../App';
+import { App } from '../app/App';
 
 describe('사용자는_목적을_위해_행동하기를_원한다', () => {
   it('페이지의 제목이 표시되어야 한다', () => {
