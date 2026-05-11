df990d9c95d1b27b4bf1161d9364d35460e6e4e6
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2026-05-11 12:38:44 +0900

    chore: setup eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier/flat'
+import importPlugin from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unicorn from 'eslint-plugin-unicorn'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -9,14 +13,43 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       globals: globals.browser,
     },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      unicorn.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            kebabCase: true,
+            pascalCase: true,
+            snakeCase: false,
+          },
+          ignore: ['^.', '^_', '^-'],
+        },
+      ],
+      'unicorn/no-null': 'off',
+      'unicorn/prefer-at': ['error', { checkAllIndexAccess: true }],
+      'unicorn/prefer-top-level-await': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
   },
+  prettierConfig,
 ])
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
+
+export default config;
