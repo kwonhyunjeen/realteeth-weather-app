609f09aa2900b8c1eb2c15540bd3dba3b602d3bf
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2026-05-12 23:44:10 +0900

    fix: resolve vercel function ESM import paths

--- api/kakao/v2/local/geo/coord2regioncode.ts
+++ api/kakao/v2/local/geo/coord2regioncode.ts
@@ -1,4 +1,4 @@
-import { proxyKakao } from '../../../_kakao';
+import { proxyKakao } from '../../../_kakao.js';
 
 export const GET = (request: Request) => {
   return proxyKakao(request, '/v2/local/geo/coord2regioncode.json');
--- api/openweathermap/data/3.0/onecall.ts
+++ api/openweathermap/data/3.0/onecall.ts
@@ -1,4 +1,4 @@
-import { proxyOpenWeatherMap } from '../../_openWeatherMap';
+import { proxyOpenWeatherMap } from '../../_openWeatherMap.js';
 
 export const GET = (request: Request) => {
   return proxyOpenWeatherMap(request, '/data/3.0/onecall');
